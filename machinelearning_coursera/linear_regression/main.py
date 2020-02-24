56582f3309d9658c35e43363164ede513c90d676
diogomatoschaves <diogo_chaves@hotmail.com>
2020-02-24 22:47:46 +0100

    Add ml package

---
+++ machinelearning_coursera/linear_regression/main.py
@@ -0,0 +1,15 @@
+
+
+class LinearRegression:
+
+    def __init__(self, strategy='gradient_descent'):
+
+        self.strategy = strategy
+
+    def fit(self, X, y):
+
+        pass
+
+    def predict(self, x):
+
+        pass
