1aed3480976d88daf0234c36744e84848553534e
diogomatoschaves <diogo_chaves@hotmail.com>
2020-03-02 21:35:48 +0100

    Add tests for regression

---
+++ ml_coursera/tests/test_regression.py
@@ -0,0 +1,13 @@
+import pytest
+from ..regression import LinearRegression, LogisticRegression
+
+
+@pytest.fixture
+def mock_das_route_matching(mocker):
+    pass
+
+
+class TestRegressions:
+    def test_linear_regression(self):
+
+        pass
